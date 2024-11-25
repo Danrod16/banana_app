e0bd5e9aed77e7a08b15d8b218167946691bed77
Daniel Rodriguez <daniel.rod93@gmail.com>
2024-11-25 10:19:44 +0800

    added devise to my project

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
+  get "/dashboard", to: "pages#dashboard", as: :dashboard
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,4 +10,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # devise_for :users
 end
